cc5f6161e2c689589134751648c27ccfa7b241dd
brian <brian.rook@gmail.com>
2019-08-05 14:30:04 -0600

    converted to lombok and yaml

--- src/main/java/com/brianrook/gcphelloworld/configuration/HelloWorldSpringConfig.java
+++ src/main/java/com/brianrook/gcphelloworld/configuration/HelloWorldSpringConfig.java
@@ -1,10 +1,14 @@
 package com.brianrook.gcphelloworld.configuration;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 
 @Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties
 public class HelloWorldSpringConfig
 {
 
